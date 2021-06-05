3ccefcbfb8c1244a16918cfa6f8f5c910852cae2
Jokubas <jokubas@shape.dk>
2021-06-05 13:52:53 +0200

    load portfolio item from a separate html file

--- src/pages/portfolio/js/pageElementManager.js
+++ src/pages/portfolio/js/pageElementManager.js
@@ -1,8 +1,23 @@
 let stickyNavWaypoint = $('.paragraphHeaderContainer .paragraphHeader');
+let portfolioListContainer = $('.portfolioContainer');
+
 
 function populateDataToPortfolioPage(data) {
-    
+    populatePortfolioList(0);
 }
+
+function populatePortfolioList(portfolioListData) {
+    //for(let item of portfolioListData) {
+        portfolioListContainer.append('<li></li>');
+        let listItem = portfolioListContainer.find('li:last-child');
+        listItem.load('src/components/portfolioListItem.html', function(){
+            // listItem.find(".timelineItemDate").html(item.date);
+            // listItem.find(".timelineItemTitle").html(item.title);
+            // listItem.find(".timelineItemDescription").html(item.description);
+            // listItem.find("img").attr("src", item.image_link);
+        });
+    //}
+};
 
 $('document').ready(
     function() {
