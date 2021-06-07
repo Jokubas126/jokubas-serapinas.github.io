849c606fecd0a08fb81bd925ebf5b0bc20723967
Jokubas <jokubas@shape.dk>
2021-06-07 14:21:25 +0200

    some main navigation adjustment and fixes. Introduced burger menu icon

--- src/res/js/basePageElementManager.js
+++ src/res/js/basePageElementManager.js
@@ -1,8 +1,16 @@
-let mainNavContainer = $('.main-nav');
+let contentContainer = $('#content');
 
 function populateBaseDataToPage(data) {
     populateFooter(data)
     populateNav(data.full_name);
+    addBackgroundImage(data.background_image);
+}
+
+function addBackgroundImage(url) {
+    let backgroundElements = $(".mainBackground");
+    for(let element of backgroundElements) {
+        $(element).css('background-image', $(element).css('background-image') + ', url(' + url + ')');
+    }
 }
 
 function populateLinks(socialMediaData) {
@@ -22,7 +30,7 @@ function populateLinks(socialMediaData) {
 }
 
 function populateFooter(data) {
-    $('#content').append(' <footer></footer>');
+    contentContainer.append(' <footer></footer>');
     $('footer').load('src/components/footer.html', function() {
         let footerNavContainer = $('.footer-nav');
         footerNavContainer.append('<li><a href="portfolio.html">Portfolio</a></li>');
@@ -34,10 +42,15 @@ function populateFooter(data) {
 }
 
 function populateNav(fullName) {
-    mainNavContainer.append('<a id="top-link" href="index.html"></a>');
-    $('#top-link').html(fullName); 
-    mainNavContainer.append('<a href="portfolio.html">Portfolio</a>');
-    mainNavContainer.append('<a href="aboutme.html">About me</a>');
+    let nav = contentContainer.find('nav');
+    nav.addClass("stickyTransparent");
+    nav.load('src/components/main_nav.html', function() {
+        let mainNavContainer = nav.find('#main-nav');
+        mainNavContainer.append('<a id="top-link" href="index.html"></a>');
+        $('#top-link').html(fullName); 
+        mainNavContainer.append('<a href="portfolio.html">Portfolio</a>');
+        mainNavContainer.append('<a href="aboutme.html">About me</a>');
+    });
 }
 
 let stickyHeaderSwitcher = function(direction) {
